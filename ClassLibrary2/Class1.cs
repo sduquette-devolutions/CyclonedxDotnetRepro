688ef58a1aa0e09156e58348be24f042d83f82e4
Sébastien Duquette <sduquette@devolutions.net>
2022-04-05 14:01:31 -0400

    Repro project for the following issue: https://github.com/CycloneDX/cyclonedx-dotnet/issues/518

---
+++ ClassLibrary2/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Amazon;
+
+namespace ClassLibrary2
+{
+    public class Class1
+    {
+        private Arn arn;
+    }
+}
